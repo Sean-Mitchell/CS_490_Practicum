88b90a10d43bc87d94c8e184aca3f9f29b2b3737
Sean-Mitchell <seanmtchl@gmail.com>
2018-09-02 16:07:48 -0500

    Scale Data
    Finally got the scaling to work correctly.  Removes unnecassary libraries.  Data is now prepared for SVM gridSearch learn (updating of hyperparameters).

--- SigcseLearn.py
+++ SigcseLearn.py
@@ -1,16 +1,12 @@
 import re
 import os
-import string as strng
-import pandas as pd
 import numpy as np
-from scipy.sparse import hstack
+import pandas as pd
+import string as strng
+from scipy.sparse import hstack, metrics, svm
 from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.feature_extraction import stop_words
-from sklearn import svm
-from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, stop_words
 from sklearn.model_selection import KFold, StratifiedKFold
-from sklearn import preprocessing as preproc
 
 
 #Thank you Internet
@@ -114,31 +110,58 @@ def MachineLearningPart(Emails, IsGoodSentenceList):
     
     #Create Series that the countVectorizer can use to create training and testing sets
     emails_train, emails_test, goodSentences_train, goodSentences_test = train_test_split(emails, goodSentences, random_state=1)
-    
+    vect.fit(emails)
+    tfidfVect.fit(emails)
     # fit and transform training into vector matrix
-    vect_emails_train_dtm = vect.fit_transform(emails_train)
-    tfid_emails_train_dtm = tfidfVect.fit_transform(emails_train)
+    vect_emails_train_dtm = vect.transform(emails_train)
+    tfid_emails_train_dtm = tfidfVect.transform(emails_train)
+    
     # transform test into test matrix
     vect_emails_test_dtm = vect.transform(emails_test)
     tfid_emails_test_dtm = tfidfVect.transform(emails_test)
-    print(goodSentences_train)
     
-    # Normalize the data
-    vect_emails_train_dtm = preproc.normalize(vect_emails_train_dtm, norm='l2')
-    vect_emails_test_dtm = preproc.normalize(vect_emails_test_dtm, norm='l2')
+    # Scale train and test vector sets
+    maxVal = vect_emails_train_dtm.max()
+    vect_emails_train_dtm = vect_emails_train_dtm/float(maxVal)
+    maxVal = tfid_emails_train_dtm.max()
+    tfid_emails_train_dtm = tfid_emails_train_dtm/float(maxVal)
     
-    tfid_emails_train_dtm = preproc.normalize(tfid_emails_train_dtm, norm='l2')
-    tfid_emails_test_dtm = preproc.normalize(tfid_emails_test_dtm, norm='l2')
-        
+    # Concatonate the columns of the training and test set
     vect_tfidf_emails_train_dtm = hstack([vect_emails_train_dtm, tfid_emails_train_dtm])
     vect_tfidf_emails_test_dtm = hstack([vect_emails_test_dtm, tfid_emails_test_dtm])
-    print(tfid_emails_test_dtm.shape)
-    print(vect_tfidf_emails_train_dtm.shape)
+    
     #Double Check shapes
     #print(emails_train_dtm)
     #print(emails_train_dtm.shape)
     #print(emails_test.shape)
     #print(emails_test_dtm.shape)
+    #print(tfid_emails_test_dtm.shape)
+    #print(vect_tfidf_emails_train_dtm.shape)
+    #print(vect_tfidf_emails_train_dtm)
+    
+    # This prints off indices of true values
+    #print([i for i, x in enumerate(goodSentences_train) if x])
+    
+    #Set up svc stuff (will modify into loop later)
+    clf = svm.SVC(C=1.0, cache_size=8000, class_weight=None, coef0=0.1,
+    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    max_iter=-1, probability=True, random_state=1, shrinking=True,
+    tol=.01, verbose=False)
+
+    clf.fit(tfid_emails_train_dtm, goodSentences_train)    
+    
+    vect_tfidf_emails_results = clf.predict(tfid_emails_test_dtm)
+    #print the accuracy is
+    print('CountVectorizer + TFIDFVectorizer Results: ')
+    print(metrics.accuracy_score(goodSentences_test, vect_tfidf_emails_results))    
+    print(metrics.precision_recall_fscore_support(goodSentences_test, vect_tfidf_emails_results))    
+    #This is a thing.  I am still uncertain how to use it
+    print(metrics.confusion_matrix(goodSentences_test, vect_tfidf_emails_results))
+        
+    accuracy_Array = []
+    accuracy_Array.append(metrics.accuracy_score(goodSentences_test, vect_tfidf_emails_results))
+    
+    
     '''
     randomStateCount = 1
     for cAmount in np.linspace(0.1,1,10):
